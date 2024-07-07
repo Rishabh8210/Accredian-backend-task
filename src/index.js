fd1ce2200bd1489b703e4e1f18af23b9a069fc73
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-07-07 11:17:03 +0530

    Added model and setup schema

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { PORT } = require('../src/config/serverConfig')
+const { PORT } = require('../src/config/serverConfig');
 
-const settingAndStartingServer = () => {
+const settingAndStartingServer = async () => {
     const app = express();
+    // const { PrismaClient } = require('@prisma/client')
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
     app.use(cors());
 
+
+
     app.listen(PORT, () => {
         console.log(`Server is running as PORT ${PORT}`);
     })
-
-    app.get('/', (req, res) => {
-        res.send("Hiii, World");
-    })
-    
 }
 settingAndStartingServer()
---
+++ src/prisma/migrations/20240707054522_add_default_wallet_value/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `user` MODIFY `wallet` DOUBLE NOT NULL DEFAULT 0.0;
